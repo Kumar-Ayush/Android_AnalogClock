c59ee6769ac0295515217a88d8bd5375b573075a
Kumar-Ayush <kumarayush205@gmail.com>
2017-02-09 00:12:09 +0530

    analog_clock 1st commit

---
+++ app/src/main/java/ayush/example/com/analog_clock/ShowClock.java
@@ -0,0 +1,15 @@
+package ayush.example.com.analog_clock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowClock extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_clock);
+       
+    }
+
+}
